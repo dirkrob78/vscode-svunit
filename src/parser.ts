a6abeea9b0c1d6183737b4a742fa30e7bcaf8adf
Dirk Robinson <dirk.robinson@amd.com>
2024-12-31 14:27:45 -0700

    Simplified version

--- src/parser.ts
+++ src/parser.ts
@@ -1,17 +1,25 @@
 import * as vscode from 'vscode';
 
-const testRe = /^\s*`SVTEST\s*\(\s*(\w+)\s*\)/;
+const svtestRe = /^\s*`SVTEST\s*\(\s*(\w+)\s*\)/;
+const svtestEndRe = /^\s*`SVTEST_END/;
 
 export const parseSVUnitTestFile = (text: string, events: {
 	onTest(range: vscode.Range, label: string): void;
 }) => {
 	const lines = text.split('\n');
 
+	let startLineNo = -1;
+	let label = "";
 	lines.forEach((line, lineNo) => {
-		const test = testRe.exec(line);
-		if (test) {
-			const [, label] = test;
-			const range = new vscode.Range(new vscode.Position(lineNo, 0), new vscode.Position(lineNo, test[0].length));
+		const svtest = svtestRe.exec(line);
+		if (svtest) {
+			[, label] = svtest;
+			startLineNo = lineNo;
+		}
+
+		const svtestEnd = svtestEndRe.exec(line);
+		if (svtestEnd && startLineNo !== -1) {
+			const range = new vscode.Range(new vscode.Position(startLineNo, 0), new vscode.Position(lineNo, line.length));
 			events.onTest(range, label);
 		}
 	});
